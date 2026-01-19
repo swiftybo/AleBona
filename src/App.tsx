a2c191eac59097da95c919a98adf443cb70e4181
BoSloth <leungjordan28@gmail.com>
2026-01-19 15:48:14 +0000

    minor changes to styling in css modules

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,10 @@ import Header from "./components/Header";
 import MemoryGallery from "./components/MemoryGallery";
 
 function App() {
-    // useEffect(() => {
-    //     const observer = new IntersectionObserver((entries) => {
-    //         const [entry] = entries;
-    //         console.log(entry);
-    //         setIsVisible(entry.isIntersecting);
-    //     }, options);
-
-    //     if (containerRef.current) observer.observe(containerRef.current);
-
-    //     return () => {
-    //         if (containerRef.current) observer.unobserve(containerRef.current);
-    //     };
-    // }, []);
-
     return (
         <main className="page">
             <Header />
             <MemoryGallery />
-            <div></div>
         </main>
     );
 }
